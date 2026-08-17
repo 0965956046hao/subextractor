6dbf5d03b43538de52c7fb7f3a7cc8aebdca9836
tinhpt <phantrongtinh31199@gmail.com>
2026-08-17 09:35:27 +0700

    move code thừa

--- frontend/src/components/shared/diagonal-stripes.tsx
+++
@@ -1,9 +0,0 @@
-export const DiagonalStripes = () => {
-  return (
-    <div className="w-full max-w-7xl mx-auto">
-      <div className="border border-border-light">
-        <div className="h-4 w-full diagonal-stripes"></div>
-      </div>
-    </div>
-  );
-};
